c49405020a5ee0ff8ef1048c166884c1fa5a5ddb
Pavel <kamushekp@mail.ru>
2018-02-05 10:12:30 +0700

    Add files via upload

---
+++ image.py
@@ -0,0 +1,16 @@
+
+class Pixel:
+    def __init__(self, r, g, b):
+        self.R = r
+        self.G = g
+        self.B = b
+
+class Image: #хранит изображение
+    def __init__(self):
+        pass
+
+    def GetPixel(self, i, j):
+        pass
+
+    def SetPixel(self, pixel, i, j):
+        pass
---
+++ opener.py
@@ -0,0 +1,7 @@
+
+class ImageFinder: #открывает картинки в директории
+        def __init__(self, searchDirectory):
+            pass
+
+        def GetAllImagesFromDirectory(selfs):
+            pass
